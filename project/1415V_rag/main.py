1e0dcc830f9a367528ca550a6fa9e850a75c01e2
림림 <hyerim23@ewhain.net>
2025-11-01 19:27:39 +0900

    Create main.py

--- project/1415V_rag/main.py
+++ project/1415V_rag/main.py
@@ -1,1 +1,16 @@
+import sys, os
+sys.path.qppend(os.path.dirname(__file__))
+from chains.qa_chain import create_hybrid_chain
 
+def main():
+    query = "비타민에서 진행했던 프로젝트에 대해 설명해주세요"
+    print(f"질문: {query}\n")
+
+    qa_chain = create_hybrid_chain(query=query)
+    answer = qa_chain.invoke({"query": query})
+
+    print("=== 최종 답변 ===")
+    print(answer["result"])
+
+if __name__ == "__main__":
+    main()
