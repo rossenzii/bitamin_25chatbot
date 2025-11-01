f8d1b5420a6229c91701789323a765fdb06eeed2
림림 <hyerim23@ewhain.net>
2025-11-01 19:11:11 +0900

    Update DATA_DIR_PATH

--- project/1415V_rag/config/settings.py
+++ project/1415V_rag/config/settings.py
@@ -4,5 +4,6 @@ import os
 load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR_PATH = os.path.join(BASE_DIR, "..", "..", "project_txt/1415V")
 FAISS_INDEX_PATH = os.path.join(BASE_DIR, "..", "rag_faiss_index")
 OPEN_API_KEY = os.getenv("OPEN_API_KEY")
