9d0e9e8fe10d262a1b781c28e2b3b1ae1c290cdf
림림 <hyerim23@ewhain.net>
2025-11-05 18:17:39 +0900

    Add bm25_retriever

--- project/project_rag/config/settings.py
+++ project/project_rag/config/settings.py
@@ -4,7 +4,10 @@ import os
 load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DATA_DIR_PATH = os.path.join(BASE_DIR, "..", "..", "..", "project_txt", "1415V")
+DATA_DIR_PATH = [
+    os.path.join(BASE_DIR, "..", "..", "..", "project_txt", "1415V"),
+    os.path.join(BASE_DIR, "..", "..", "..", "project_txt", "1415S")
+]
 FAISS_INDEX_PATH = os.path.join(BASE_DIR, "..", "rag_faiss_index")
 OPEN_API_KEY = os.getenv("OPEN_API_KEY")
 HUGGINGFACEHUB_API_TOKEN = os.getenv("HUGGINGFACEHUB_API_TOKEN")
